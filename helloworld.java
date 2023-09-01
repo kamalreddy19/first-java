03d8e52b6e81a9b9bda50b56bc979a3f5c9a5665
kamal <kamal@google.com>
2023-09-01 02:03:58 +0000

    Monday

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
       System.out.println("Hello World");
-      System.out.println("welcome to java);
+      System.out.println("welcome to java from clone);
     }
   }
